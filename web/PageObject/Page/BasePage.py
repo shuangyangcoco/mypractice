1ca7e0af83a4a1896cc5a272418576ea3d857792
Shuang Yang <Shuang.Yang@watchguard.com>
2019-06-24 19:48:56 +0800

    -afix(BasePage.py):add wait function

--- web/PageObject/Page/BasePage.py
+++ web/PageObject/Page/BasePage.py
@@ -1,5 +1,10 @@
-
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class BasePage(object):
     def __init__(self, driver):
         self.driver = driver
+
+    def display_wait(self, drvier, local, value, message, times=10, interval=0.5, exception=None):
+        WebDriverWait(drvier, times, interval, exception).until(
+            EC.presence_of_element_located((local, value)), message=message)
