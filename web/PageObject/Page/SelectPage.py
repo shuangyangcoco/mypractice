41cd877ce5947088f3eecc5db47b4013d615be9f
Shuang Yang <Shuang.Yang@watchguard.com>
2019-06-24 19:50:35 +0800

    -afix(SelectPage.py):add get current stock price API

--- web/PageObject/Page/SelectPage.py
+++ web/PageObject/Page/SelectPage.py
@@ -2,10 +2,21 @@ from web.PageObject.Page.BasePage import BasePage
 
 
 class SelectPage(BasePage):
-
     def addSelect(self, stock_name, stock_type):
         self.driver.find_element_by_css_selector('.optional .search__dropdown input').sendkey(stock_name)
-        self.driver.find_element_by_xpath("//*[@class='search__dropdown__list']//*[contains(text(),'01688')]").click()
+        self.driver.find_element_by_xpath("//*[@class='search__dropdown__list']//*[contains(text(),{})]".format(stock_type)).click()
         return self
+
+    def getCurrentPrice(self, stockname):
+        self.display_wait(
+            self.driver,
+            By.XPATH,
+            '//*[@class="optional__tb optional_stocks"]//*[contains(text(),"01688")]',
+            "not find this elements").click()
+        current_price = self.driver.find_element_by_xpath('//*[@class="optional__tb optional_stocks"]//*[contains(text(),"01688")]/../../../td[2]').text()
+        return float(current_price)
+
+
+
     
 
